48c887601534de95fe9fd67cb30ba6fb43cc1977
vaibhav2.gupta <vaibhav2.gupta@paytmbank.com>
2022-12-03 01:29:22 +0530

    Coroutines context

--- src/main/kotlin/Context.kt
+++ src/main/kotlin/Context.kt
@@ -1,2 +1,21 @@
-class Context {
+import kotlinx.coroutines.*
+
+fun main() {
+    runBlocking {
+        launch(CoroutineName("jordiee01")) {
+            print(coroutineContext[CoroutineName])
+        }
+        CoroutineScope(CoroutineName("jordiee02")).launch {
+            println(coroutineContext[CoroutineName])
+            launch {
+                println(coroutineContext[CoroutineName])
+            }
+        }
+        GlobalScope.launch(CoroutineName("jordiee03")) {
+            println(coroutineContext[CoroutineName])
+            launch {
+                println(coroutineContext[CoroutineName])
+            }
+        }
+    }
 }
